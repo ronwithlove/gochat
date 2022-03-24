e760a5a1ddcdf2829dd2713487fe4ae3d37cc47d
ronwithlove <shiyiming@outlook.com>
2022-03-24 15:11:47 +0800

    add history in chat window

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,25 +3,39 @@ import React, { Component } from "react";
 import "./App.css";
 import { connect, sendMsg } from "./api";
 import Header from './components/Header/Header';
+import ChatHistory from './components/ChatHistory/ChatHistory';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        connect();
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatHistory: []
     }
+  }
 
-    send() {
-        console.log("hello");
-        sendMsg("hello");
-    }
+  componentDidMount() {
+    connect((msg) => {
+      console.log("New Message")
+      this.setState(prevState => ({
+        chatHistory: [...this.state.chatHistory, msg]
+      }))
+      console.log(this.state);
+    });
+  }
 
-    render() {
-      return (
-          <div className="App">
-              <Header />
-              <button onClick={this.send}>Hit</button>
-          </div>
-      );
+  send() {
+    console.log("hello");
+    sendMsg("hello");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <ChatHistory chatHistory={this.state.chatHistory} />
+        <button onClick={this.send}>Hit</button>
+      </div>
+    );
   }
 }
 

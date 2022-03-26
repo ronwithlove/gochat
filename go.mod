cc0fd6e302295f4c5bada9c36b26ca2821edc411
ronwithlove <shiyiming@outlook.com>
2022-03-26 19:39:53 +0800

    ws works, but only one client able to online

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/gochat
 
 go 1.16
 
-require github.com/gorilla/websocket v1.5.0 // indirect
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/gorilla/websocket v1.5.0
+)
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/gochat/pkg/mywebsocket"
 	"net/http"
 )
@@ -30,6 +31,34 @@ func setupRoutes() {
 
 func main() {
 	fmt.Println("Distributed Chat App v0.02")
-	setupRoutes()
-	http.ListenAndServe(":8080", nil)
+	//setupRoutes()
+	//http.ListenAndServe(":8080", nil)
+
+	r := Setup()
+	r.Run()
+
+}
+
+func Setup() *gin.Engine {
+	r := gin.Default()
+
+	r.GET("/ws", IndexHandler)
+
+	return r
+}
+
+func IndexHandler(c *gin.Context) {
+	pool := mywebsocket.NewPool()
+	go pool.Start()
+
+	fmt.Println("WebSocket Endpoint Hit")
+	conn, err := mywebsocket.Upgrade(c.Writer, c.Request)
+	if err != nil {
+		fmt.Fprintf(c.Writer, "%+v\n", err)
+	}
+
+	client:=mywebsocket.NewClient(conn,pool)
+
+	pool.Register <- client
+	client.Read()
 }
